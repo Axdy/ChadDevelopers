d9bf4d45cb3cbe578577555c8e14f4f62bf2a06c
2176708c <2176708c@student.gla.ac.uk>
2017-10-28 16:58:45 +0100

    created a basic polly app

---
+++ polly_emotions.py
@@ -0,0 +1,11 @@
+#Creates an mp3 file of the text inserted and returns it
+import boto3
+from pygame import mixer
+import os
+
+polly = boto3.client('polly')
+spoken_text = polly.synthesize_speech(Text= 'Hey! You look happy. Here is a playlist for your moods.', OutputFormat= 'mp3', VoiceId = 'Joanna')
+
+with open('output.mp3', 'wb') as f:
+    f.write(spoken_text['AudioStream'].read())
+    f.close()
