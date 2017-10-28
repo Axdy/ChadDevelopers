798be191cf095510f81322187b4a5bb098553847
aeroniero33 <justthisthing@gmail.com>
2017-10-28 22:20:42 +0100

    Added the default option on polly

--- polly_emotions.py
+++ polly_emotions.py
@@ -4,7 +4,11 @@ import os
 
 def make_emotion_speech(emotion):
     polly = boto3.client('polly')
-    spoken_text = polly.synthesize_speech(Text= "Hey! You look " + emotion + ". Here is a playlist for your mood.", OutputFormat= 'mp3', VoiceId = 'Joanna')
+    if emotion == "DEFAULT":
+        text = "Hey! I don't know what you're feeling, but here's a playlist anyway."
+    else:
+        text = "Hey! You look " + emotion + ". Here is a playlist for your mood."
+    spoken_text = polly.synthesize_speech(Text= text, OutputFormat= 'mp3', VoiceId = 'Joanna')
 
     with open('output.mp3', 'wb') as f:
         f.write(spoken_text['AudioStream'].read())
